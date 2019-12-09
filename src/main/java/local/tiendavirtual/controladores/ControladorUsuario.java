570b78496b21534adf4b5aecac84b4a43ed89cb5
wilson <willyson43@hotmail.com>
2019-12-09 15:59:26 -0500

    CRUD de usuarios y categorias terminado

--- src/main/java/local/tiendavirtual/controladores/ControladorUsuario.java
+++ src/main/java/local/tiendavirtual/controladores/ControladorUsuario.java
@@ -1,6 +1,7 @@
 package local.tiendavirtual.controladores;
 
 import java.util.List;
+import java.util.Optional;
 import javax.validation.Valid;
 import local.tiendavirtual.modelos.Usuario;
 import local.tiendavirtual.repositorios.UsuarioRepositorio;
@@ -29,22 +30,24 @@ public class ControladorUsuario {
     }
     
     @PostMapping("/usuarios")
-    public Usuario agregar(@Valid @RequestBody Usuario producto) {
-        return repositorio.save(producto);
+    public Usuario agregar(@Valid @RequestBody Usuario usuario) {
+        return repositorio.save(usuario);
     }
     
-    @PutMapping("/usuarios")
-    public Usuario actualizar(@Valid @RequestBody Usuario producto) {
-        return repositorio.save(producto);
+    @PutMapping("/usuarios/{correo}")
+    public Usuario actualizar(@PathVariable("correo") String correo,
+                              @Valid @RequestBody Usuario usuario) {
+        usuario.setCorreo(correo);
+        return repositorio.save(usuario);
     }
     
     @GetMapping("/usuarios/{correo}")
     public Usuario buscar(@PathVariable("correo") String correo) {
-        return repositorio.findById(correo).orElse(null);
+        return repositorio.findById(correo + ".").get();
     }
     
     @DeleteMapping("/usuarios/{correo}")
     public void eliminar(@PathVariable("correo") String correo) {
-        repositorio.deleteById(correo);
+        repositorio.deleteById(correo + ".");
     }
 }
